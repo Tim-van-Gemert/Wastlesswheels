c914f7a92a23804536f59d24281301406c58e353
Tim van Gemert <tim@upside.nl>
2023-11-26 17:12:56 +0100

    debugging gtm

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,11 +9,11 @@ function MyDocument() {
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer',${process.env.GTM});`}}>
+            })(window,document,'script','dataLayer', 'GTM-W2T7DJKT' );`}}>
           </Script>
       </Head>
       <body>
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM}"
+          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W2T7DJKT'"
           height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         <Main />
         <NextScript />
