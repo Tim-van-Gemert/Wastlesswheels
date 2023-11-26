00f9fdbd0095c0b5348031d05332eb6d49e2d1db
Tim van Gemert <tim@upside.nl>
2023-11-26 16:49:58 +0100

    debugging

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,26 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// Import necessary modules from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import { useEffect } from 'react';
 
 export default function Document() {
+  useEffect(() => {
+    // Execute GTM script on the client side
+    (function (w, d, s, l, i) {
+      w[l] = w[l] || [];
+      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+      var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
+      j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', process.env.GTM);
+  }, []);
+
   return (
     <Html lang="en">
       <Head />
-      <head>
-          {
-          <script dangerouslySetInnerHTML={{
-              __html: `(function (w, d, s, l, i) {
-                      w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-                      var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-                      j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-                  })(window, document, 'script', 'dataLayer', ${process.env.GTM});`
-          }}></script>
-          }
-      </head>
       <body>
-          <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM}`}
-          height="0" width="0"></iframe></noscript>
+        <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM}`} height="0" width="0"></iframe></noscript>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
