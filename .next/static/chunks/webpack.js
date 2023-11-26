76f084856490ce5a7182a5709c05fe870de1c206
Tim van Gemert <tim@upside.nl>
2023-11-26 19:11:48 +0100

    change landingpage url

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e68d4b1a30330481"; }
+/******/ 		__webpack_require__.h = function() { return "2af18e3f87c803a5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
