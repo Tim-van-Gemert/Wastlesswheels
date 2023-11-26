16d079f93e6586fcbfb83e8d2ca399f92e99e306
Tim van Gemert <tim@upside.nl>
2023-11-26 16:30:21 +0100

    fix style error

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8ced11f1a1fdcbd8"; }
+/******/ 		__webpack_require__.h = function() { return "1220c1b3669feacb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
