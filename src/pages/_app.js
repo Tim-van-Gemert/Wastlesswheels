f68c581685251ee2cfb4f19b3458d3e64d8d63b7
Tim van Gemert <tim@upside.nl>
2023-11-26 16:42:33 +0100

    debugging

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,8 +3,6 @@ import Footer from '@/components/footer'
 import Header from '@/components/header'
 import Script from 'next/script'
 import { useEffect } from 'react';
-import { initializeGoogleTagManager } from './googleTagManager';
-
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
@@ -32,13 +30,13 @@ export default function App({ Component, pageProps }) {
     }
   ]
 
-  useEffect(() => {
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', 'GTM-W2T7DJKT');
-  }, []);
+  // useEffect(() => {
+  //   (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  //   new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  //   j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  //   'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  //   })(window,document,'script','dataLayer', 'GTM-W2T7DJKT');
+  // }, []);
 
 
   return <>
