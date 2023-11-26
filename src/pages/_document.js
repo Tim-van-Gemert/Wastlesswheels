cbca3b9a99d20c9fc119d5ef091a7ed638927e3b
Tim van Gemert <tim@upside.nl>
2023-11-26 16:52:21 +0100

    debugging

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,26 +1,35 @@
-// Import necessary modules from 'next/document'
-import { Html, Head, Main, NextScript } from 'next/document';
-import { useEffect } from 'react';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  useEffect(() => {
-    // Execute GTM script on the client side
-    (function (w, d, s, l, i) {
-      w[l] = w[l] || [];
-      w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-      var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l !== 'dataLayer' ? '&l=' + l : '';
-      j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', process.env.GTM);
-  }, []);
-
+function MyDocument() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=%27+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', ${process.env.GTM});
+            `,
+          }}
+        />
+      </Head>
       <body>
-        <noscript><iframe src={`https://www.googletagmanager.com/ns.html?id=${process.env.GTM}`} height="0" width="0"></iframe></noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GTM}"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
+export default MyDocument;
