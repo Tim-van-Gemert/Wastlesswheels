520a95b2e1b6458e6f69c8a1647be90246656d0a
Tim van Gemert <tim@upside.nl>
2023-12-06 16:24:04 +0100

    bug-fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d05a4aae0b7bdfd4"; }
+/******/ 		__webpack_require__.h = function() { return "367c61cfe342c9c9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.3b26d49107ca4e24.hot-update.js
+++ .next/static/webpack/webpack.3b26d49107ca4e24.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "d05a4aae0b7bdfd4"; }
+/******/ 	__webpack_require__.h = function() { return "367c61cfe342c9c9"; }
 /******/ }();
 /******/ 
 /******/ }
